41456ae923efaf816dd50e51ee5c3f1ebdb4d10a
messaourghada <ghada.messaour.dz@viacesi.fr>
2017-11-06 14:12:08 +0100

    Merge remote-tracking branch 'origin/master'
    #Conflicts:
    # main.c

--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition(int a, int b)
-{
-    return a+b;
-}
+
 int main()
 {
-<<<<<<< HEAD
-printf("a+b=%d\n", addition(1,4));
-=======
-printf("L’addition de a et de b vaut %d\n", addition(1,4)); 
->>>>>>> 962133a81600ffec6cfd86d84ba717da6ee868e8
+
+printf("L’addition de a et de b vaut %d\n", addition(1,4));
+
      return 0;
 
 }
